1515ac7bec7c1aad8ab45ff20fdd8a6fc3a9eef4
nuttanan355 <nuttanan355@gmail.com>
2023-01-03 00:46:26 +0700

    (JJ) Update UI V1.2

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App_j';
 import reportWebVitals from './reportWebVitals';
-// import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
